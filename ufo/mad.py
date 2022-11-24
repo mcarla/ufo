1592bc5ca5323d358baa31ae4f95244086b89b46
mcarla <mcarla@member.fsf.org>
2022-11-24 10:56:29 +0100

    Added string parsing ability in mad input file, now parameters can be also OpenCL code: usefull for aperture definition

--- ufo/mad.py
+++ ufo/mad.py
@@ -13,8 +13,8 @@ Token = collections.namedtuple('Token', ['type', 'value'])
 
 def tokenize(mad_file):
     token_specification = [
-                             #Match scalar   | match vector {a, b, c...}
-        ('NUMERIC',         r'(-?\d+(\.\d*)?)|(\s*\{(-?\d+(\.\d*)?\s*,?\s*)*\})'),
+                             #Match scalar   | match vector {a, b, c...}       | string
+        ('PARAMETER',       r'(-?\d+(\.\d*)?)|(\s*\{(-?\d+(\.\d*)?\s*,?\s*)*\})|(".*")'),
         ('OPEN',            r'\('),                     #
         ('CLOSE',           r'\)'),                     #
         ('COLON',           r':'),                      #
@@ -34,8 +34,10 @@ def tokenize(mad_file):
         kind = mo.lastgroup
         value = mo.group()
 
-        if kind == 'NUMERIC':
-            if '{' in value: #Is a vector
+        if kind == 'PARAMETER':
+            if '"' in value: #Is a string
+                value = value.replace('"', '').replace('"', '')
+            elif '{' in value: #Is a vector
                 value = value.replace('{', '').replace('}', '').split(',')
                 value = [float(n) for n in value]
             else: #Is a scalar
@@ -62,7 +64,7 @@ def parse_element(tokenizer):
         parameter_name = token.value
 
         token = pop_or_die(tokenizer, 'ASSIGN')
-        token = pop_or_die(tokenizer, 'NUMERIC')
+        token = pop_or_die(tokenizer, 'PARAMETER')
         parameters[parameter_name] = token.value
 
     return parameters
@@ -106,7 +108,8 @@ def load(path, flatten=True):
         'RBEND'     : lambda label, L=0., ANGLE=0., K1=0., E1=0., E2=0.: ufo.Rbend(label, length=L, angle=ANGLE, k1=K1, e1=E1, e2=E2),
         'SEXTUPOLE' : lambda label, L=0., K2=0., K2S=0.:   ufo.Sextupole(label, length=L, k2=K2, k2s=K2S),
         'OCTUPOLE'  : lambda label, L=0., K3=0., K3S=0.:   ufo.Octupole(label, length=L, k3=K3, k3s=K3S),
-        'MULTIPOLE' : lambda label, KNL=[], KSL=[]:        ufo.Multipole(label, knl=KNL, ksl=KSL)
+        'MULTIPOLE' : lambda label, KNL=[], KSL=[]:        ufo.Multipole(label, knl=KNL, ksl=KSL),
+        'APERTURE'  : lambda label, A=0:                   ufo.Aperture(label, aperture=A)
     }
 
     try:
